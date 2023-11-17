2b3be23233830d9396683756f98a57433c4e3aa4
jo <chojh3123@gmail.com>
2023-11-17 22:43:24 +0900

    v1

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/hello/v1")
     public String hello(){
         return "hello";
     }
